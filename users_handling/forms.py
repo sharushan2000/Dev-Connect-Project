d273e27bb684099e397f04cc92f89b6162ec5743
sharushan2000 <sharushan0000@gmail.com>
2024-02-28 18:41:45 -0500

    user registration added

---
+++ users_handling/forms.py
@@ -0,0 +1,19 @@
+from typing import Any
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with that email already exists.")
+        return email
--- users_handling/views.py
+++ users_handling/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
     return render(request ,'users_handling/login.html' ,{})
 
 def signup(request):
-    return render(request ,'users_handling/signup.html' ,{})
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, your account has been created successfully. Please log in.')
+            return redirect('home:home')
+        else:
+            # Form is not valid, so render the form again with validation errors
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = RegisterForm()
+    
+    # This return statement will handle both new (GET) requests and invalid (POST) submissions
+    return render(request, 'users_handling/register.html', {'form': form})
